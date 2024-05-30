f4a530be59973901092ec9a919b57a13e4045d64
viks1102 <viks_1102@yahoo.com>
2024-05-30 23:48:42 +0100

    wip

--- prod/terraform/subnet.tf
+++ prod/terraform/subnet.tf
@@ -1,17 +1,17 @@
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/compute_subnetwork
 resource "google_compute_subnetwork" "private2" {
   name                     = "private2"
-  ip_cidr_range            = "10.0.0.0/18"
+  ip_cidr_range            = "10.60.0.0/20"
   region                   = "us-central1"
   network                  = "k8s-vpc-global"
   private_ip_google_access = true
 
   secondary_ip_range {
     range_name    = "k8s-pod-range"
-    ip_cidr_range = "10.48.0.0/14"
+    ip_cidr_range = "10.28.0.0/14"
   }
   secondary_ip_range {
     range_name    = "k8s-service-range"
-    ip_cidr_range = "10.52.0.0/20"
+    ip_cidr_range = "10.27.0.0/20"
   }
 }
