761d3fd4848aff51efdee3119a819afa0528d19d
viks1102 <viks_1102@yahoo.com>
2024-05-30 21:38:24 +0100

    wip

--- prod/terraform/main.tf
+++ prod/terraform/main.tf
@@ -1,50 +1,50 @@
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster
-resource "google_container_cluster" "gke-cluster-prod" {
-  name                     = "gke-cluster-prod"
-  location                 = "us-central1-c"
-  remove_default_node_pool = true
-  initial_node_count       = 1
-  network                  = "k8s-vpc-global"
-  subnetwork               = "private"
-  logging_service          = "logging.googleapis.com/kubernetes"
-  monitoring_service       = "monitoring.googleapis.com/kubernetes"
-  networking_mode          = "VPC_NATIVE"
+# # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/container_cluster
+# resource "google_container_cluster" "gke-cluster-prod" {
+#   name                     = "gke-cluster-prod"
+#   location                 = "us-central1-c"
+#   remove_default_node_pool = true
+#   initial_node_count       = 1
+#   network                  = "k8s-vpc-global"
+#   subnetwork               = "private"
+#   logging_service          = "logging.googleapis.com/kubernetes"
+#   monitoring_service       = "monitoring.googleapis.com/kubernetes"
+#   networking_mode          = "VPC_NATIVE"
 
-  # Disable deletion protection
-  deletion_protection = false
-  # Optional, if you want multi-zonal cluster
-  node_locations = [
-    "us-central1-a",  # Secondary zone
-    "us-central1-b"   # Another secondary zone
-  ]
+#   # Disable deletion protection
+#   deletion_protection = false
+#   # Optional, if you want multi-zonal cluster
+#   node_locations = [
+#     "us-central1-a",  # Secondary zone
+#     "us-central1-b"   # Another secondary zone
+#   ]
 
-  addons_config {
-    http_load_balancing {
-      disabled = true
-    }
-    horizontal_pod_autoscaling {
-      disabled = false
-    }
-  }
+#   addons_config {
+#     http_load_balancing {
+#       disabled = true
+#     }
+#     horizontal_pod_autoscaling {
+#       disabled = false
+#     }
+#   }
 
-  release_channel {
-    channel = "REGULAR"
-  }
+#   release_channel {
+#     channel = "REGULAR"
+#   }
 
-  workload_identity_config {
-    workload_pool = "dtonic-demo-k8s.svc.id.goog"
-  }
+#   workload_identity_config {
+#     workload_pool = "dtonic-demo-k8s.svc.id.goog"
+#   }
 
-  ip_allocation_policy {
-    cluster_secondary_range_name  = "10.48.0.0/14"
-    services_secondary_range_name = "10.52.0.0/20"
-  }
+#   ip_allocation_policy {
+#     cluster_secondary_range_name  = "10.48.0.0/14"
+#     services_secondary_range_name = "10.52.0.0/20"
+#   }
 
-  private_cluster_config {
-    enable_private_nodes    = true
-    enable_private_endpoint = false
-    master_ipv4_cidr_block  = "172.16.0.0/28"
-  }
+#   private_cluster_config {
+#     enable_private_nodes    = true
+#     enable_private_endpoint = false
+#     master_ipv4_cidr_block  = "172.16.0.0/28"
+#   }
 
 
-}
+# }
