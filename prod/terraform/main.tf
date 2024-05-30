6153c041f4dcc0badef0b927776b3952260b64dc
viks1102 <viks_1102@yahoo.com>
2024-05-30 22:52:56 +0100

    wip

--- prod/terraform/main.tf
+++ prod/terraform/main.tf
@@ -5,7 +5,7 @@ resource "google_container_cluster" "gke-cluster-prod" {
   remove_default_node_pool = true
   initial_node_count       = 1
   network                  = "k8s-vpc-global"
-  subnetwork               = "private"
+  subnetwork               = google_compute_subnetwork.private.self_link
   logging_service          = "logging.googleapis.com/kubernetes"
   monitoring_service       = "monitoring.googleapis.com/kubernetes"
   networking_mode          = "VPC_NATIVE"
