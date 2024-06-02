7d21445dd7b24874ff1dd7f5204a694856b10437
viks1102 <viks_1102@yahoo.com>
2024-06-02 22:40:16 +0100

    test push

--- nonprod/terraform/output.tf
+++ nonprod/terraform/output.tf
@@ -1,14 +1,14 @@
-output "kubernetes_service_account_name" {
-  description = "The name of the Kubernetes service account"
-  value       = kubernetes_service_account.k8s_service_account.metadata[0].name
-}
-
-output "google_service_account_email" {
-  description = "The email of the Google Service Account"
-  value       = google_service_account.gsa.email
-}
-
-# output "gke_cluster_name" {
-#   description = "The name of the GKE cluster"
-#   value       = google_container_cluster.primary.name
+# output "kubernetes_service_account_name" {
+#   description = "The name of the Kubernetes service account"
+#   value       = kubernetes_service_account.k8s_service_account.metadata[0].name
 # }
+
+# output "google_service_account_email" {
+#   description = "The email of the Google Service Account"
+#   value       = google_service_account.gsa.email
+# }
+
+# # output "gke_cluster_name" {
+# #   description = "The name of the GKE cluster"
+# #   value       = google_container_cluster.primary.name
+# # }
